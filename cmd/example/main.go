5536d6e99ea1ee684a2081ca3ed93c66075d5c95
Illyakravchuk <ilakrawchyk18@gmail.com>
2023-03-19 23:38:34 +0200

    added tests to the prefixToInfix function

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	lab2 "github.com/Illyakravchuk/lab_"
+	lab2 "github.com/roman-mazur/architecture-lab-2"
 )
 
 var (
--- implementation_test.go
+++ implementation_test.go
@@ -1,23 +1,81 @@
 package lab2
-
 import (
-	"fmt"
+	. "gopkg.in/check.v1"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
-)
-
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
-	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
+	"errors"
+	)
+	
+	func Test(t *testing.T) { TestingT(t) }
+	
+	type prefixToInfixSuite struct{}
+	
+	var _ = Suite(&prefixToInfixSuite{})
+	
+	func (s *prefixToInfixSuite) TestEmptyInput(c *C) {
+	res, err := prefixToInfix("")
+	c.Assert(res, Equals, "")
+	c.Assert(err, DeepEquals, errors.New("invalid expression"))
 	}
-}
+	
+	func (s *prefixToInfixSuite) TestSingleNumber(c *C) {
+	res, err := prefixToInfix("5")
+	c.Assert(res, Equals, "5")
+	c.Assert(err, IsNil)
+	}
+	
+	func (s *prefixToInfixSuite) TestInvalidInput(c *C) {
+	res, err := prefixToInfix("+ 2")
+	c.Assert(res, Equals, "")
+	c.Assert(err, DeepEquals, errors.New("invalid expression"))
+	}
+	
+	func (s *prefixToInfixSuite) TestSimplePrefixExpression(c *C) {
+	res, err := prefixToInfix("+ 2 3")
+	c.Assert(res, Equals, "2 + 3")
+	c.Assert(err, IsNil)
+	}
+	
+	func (s *prefixToInfixSuite) TestNestedPrefixExpression(c *C) {
+	res, err := prefixToInfix("+ * 2 3 4")
+	c.Assert(res, Equals, "2 * 3 + 4")
+	c.Assert(err, IsNil)
+	}
+	
+	func (s *prefixToInfixSuite) TestComplexPrefixExpression(c *C) {
+	res, err := prefixToInfix("+ * / 12 3 3 5")
+	c.Assert(res, Equals, "12 / 3 * 3 + 5")
+	c.Assert(err, IsNil)
+	}
+	
+	func (s *prefixToInfixSuite) TestPowerOperator(c *C) {
+	res, err := prefixToInfix("^ 2 3")
+	c.Assert(res, Equals, "2 ^ 3")
+	c.Assert(err, IsNil)
+	}
+	
+	func (s *prefixToInfixSuite) TestParentheses(c *C) {
+	res, err := prefixToInfix("+ * 2 3 4")
+	c.Assert(res, Equals, "2 * 3 + 4")
+	c.Assert(err, IsNil)
+	
+	res, err = prefixToInfix("+ * 2 3 - 4 5")
+	c.Assert(res, Equals, "2 * 3 + (4 - 5)")
+	c.Assert(err, IsNil)
+	
+	res, err = prefixToInfix("+ * 2 / 6 2 - 4 5")
+	c.Assert(res, Equals, "2 * (6 / 2) + (4 - 5)")
+	c.Assert(err, IsNil)
 
-func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
-	fmt.Println(res)
+	res, err = prefixToInfix("+ * ^ 9 2 5 - 22 / 10 2")
+	c.Assert(res, Equals, "9 ^ 2 * 5 + (22 - (10 / 2))")
+	c.Assert(err, IsNil)
 
-	// Output:
-	// 2 2 +
-}
+	res, err = prefixToInfix("- / * 5 8 2 + 10 2")
+	c.Assert(res, Equals, "(5 * 8) / 2 - (10 + 2)")
+	c.Assert(err, IsNil)
+
+	res, err = prefixToInfix("+ - / * 6 2 12 2 * 3 2")
+	c.Assert(res, Equals, "6 * 2 / 12 - 2 + (3 * 2)")
+	c.Assert(err, IsNil)
+
+	}
